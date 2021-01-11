844fb4fcdb707e0fab9b7593e910fc8136473cb1
YK <850435961@qq.com>
2021-01-11 17:06:32 +0800

    修改pom文件

--- src/test/java/org/caesar/example/TestCreateSign.java
+++ src/test/java/org/caesar/example/TestCreateSign.java
@@ -1,7 +1,11 @@
 package org.caesar.example;
 
+import org.springframework.http.ResponseEntity;
 import org.testng.annotations.Test;
 import org.caesar.example.utils.CreateSign;
+
+import org.springframework.web.client.RestTemplate;
+
 
 public class TestCreateSign {
     @Test
@@ -9,5 +13,14 @@ public class TestCreateSign {
     {
         System.out.println(new CreateSign().sign("hey girl"));
 
+    }
+
+    @Test
+    public void testGet()
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://10.130.210.87:8844/system/version", String.class);
+        System.out.println(responseEntity);
+        System.out.println(responseEntity.getStatusCode().value());
     }
 }
